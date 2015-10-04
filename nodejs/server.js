f39c1f39006935887e3ef2ab5d28c28b1a92c022
Barry <log525lg100@gmail.com>
2015-10-04 13:21:56 +1100

    add nodejs example code
    add nodejs example code

--- nodejs/server.js
+++ nodejs/server.js
@@ -15,8 +15,38 @@ function tcpServer(){
 	});
 	server.listen(20080, '127.0.0.1');
 }
-var indexPage="/index";
+
 var users={};
+var urls={};
+var roomPeople={};
+function User(id,name,room) {
+   this.id = id;
+   this.name = name;
+   this.room = room;
+
+   this.getId = function() {
+      return id;
+   };
+   this.getName = function() {
+      return name;
+   };
+   this.getRoom = function() {
+      return room;
+   };
+}
+urls["/index"]="/index.html";
+urls["/avatar"]="/Avatar Content.html";
+urls["/guess"]="/Guess.html";
+function includeResourceFile(appi,resourcePath){
+	appi.get(resourcePath, function(req, res){
+	  res.sendFile(__dirname + req.originalUrl);
+	});
+}
+function responseView(appi,url){
+	appi.get(url, function(req, res){
+	  res.sendFile(__dirname + urls[url]);
+	});
+}
 function simpleChat(){
 	var app = require('express')();
 	var http = require('http').Server(app);
@@ -25,32 +55,46 @@ function simpleChat(){
 	app.get('/', function(req, res){
 	  res.send('<h1>Hello world</h1>');
 	});
-	app.get(indexPage, function(req, res){
-	  res.sendFile(__dirname + '/view/index.html');
-	});
+	responseView(app,"/index");
+	responseView(app,"/avatar");
+	responseView(app,"/guess");
+	includeResourceFile(app,"/materialize/*");
+	includeResourceFile(app,"/jsmodule/*");
+	includeResourceFile(app,"/images/*");
 	
 	io.on('connection', function(socket){
 		console.log(socket.id+' a user connected');
 		socket.on('user nickname', function(msg){
 			console.log('user nickname: ' + msg);
-			users[socket.id]=msg;
-			socket.broadcast.emit('new user',msg+' connected');
+			users[socket.id]=new User(socket.id,msg.nickname,msg.roomNum);
+			socket.join(msg.roomNum);
+			if (roomPeople[msg.roomNum]==undefined){
+				roomPeople[msg.roomNum]=1;
+				socket.broadcast.to(msg.roomNum).emit('new user',msg.nickname+' connected');
+			}else if (roomPeople[msg.roomNum]<2){
+				roomPeople[msg.roomNum]=2;
+				socket.broadcast.to(msg.roomNum).emit('new user',msg.nickname+' connected');
+			}else{
+				socket.emit('room full',"choose another one");
+			}
 		});
 		socket.on('chat message', function(msg){
 			console.log('message: ' + msg);
-			var nickname=users[socket.id];
-			//io.emit('chat message', nickname+' : '+msg,{ for: 'everyone' });
-			socket.broadcast.emit('chat message',nickname+' : '+msg);
+			var user=users[socket.id];
+			socket.broadcast.to(user.getRoom()).emit('chat message',users[socket.id].getName()+' : '+msg);
 		});
 		socket.on('user typing', function () {
-			socket.broadcast.emit('typing',users[socket.id]+' is typing');
+			var user=users[socket.id];
+			socket.broadcast.to(user.getRoom()).emit('typing',users[socket.id].getName()+' is typing');
 		});
 		socket.on('stop typing', function () {
-			socket.broadcast.emit('stop typing',true);
+			var user=users[socket.id];
+			socket.broadcast.to(user.getRoom()).emit('stop typing',true);
 		});
 		socket.on('disconnect', function () {
+			var user=users[socket.id];
 			console.log(socket.id+' is disconnected');
-			socket.broadcast.emit('user leave',users[socket.id]+' leave');
+			socket.broadcast.to(user.getRoom()).emit('user leave',users[socket.id].getName()+' leave');
 		});
 	});
 	
